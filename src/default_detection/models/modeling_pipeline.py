6d54c52183254f0d01d71145bc5052eceb6bf782
Przemek <arl3nu@gmail.com>
2025-05-24 23:11:03 +0200

    refactor: rename experiment variable for consistency in PocessModeling class

--- src/default_detection/models/modeling_pipeline.py
+++ src/default_detection/models/modeling_pipeline.py
@@ -89,7 +89,7 @@ class PocessModeling:
         self.parameters = self.config.parameters
         self.catalog_name = self.config.catalog_name
         self.schema_name = self.config.schema_name
-        self.experiment_name = f"{self.config.schema_name}_Default_Detection_Experiment"
+        self.experiment_name = f"{self.config.schema_name}_dd_experiment"
         self.tags = tags.dict()
         self.code_paths = code_paths
 
